1a5bf22641724250c9c75d798660c35b09b1f2e4
nivancheva <nedyalka.tsintsarova@gmail.com>
2024-05-09 15:17:55 +0300

    functionality

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,32 @@
 <script setup>
+import { ref, onMounted, reactive } from "vue";
+
+const hourHand = ref("");
+const minuteHand = ref("");
+const secondHand = ref("");
+const currentDate = ref(new Date());
 
 
+function setRotation(element,rotationRatio) {
+  return reactive({
+    '--rotation': rotationRatio.value * 360
+  })
+}
+onMounted(() => {
+  setInterval(() => {
+
+    currentDate.value = new Date();
+    const secondsRatio = currentDate.value.getSeconds() / 60;
+    const minutesRatio = (secondsRatio + currentDate.value.getMinutes()) / 60;
+    const hoursRatio = (minutesRatio + currentDate.value.getHours()) / 12; 
+
+    setRotation(secondHand.value, secondsRatio);
+    setRotation(minuteHand.value, minutesRatio);
+    setRotation(hourHand.value, hoursRatio);
+
+    
+  }, 1000);
+});
 
 
 </script>
@@ -8,9 +34,9 @@
 <template>
  <div class="clock-dial">
     <div class="point"></div>
-    <div class="hand hour"></div>
-    <div class="hand minute"></div>
-    <div class="hand second"></div>
+    <div class="hand hour" ref="hourHand" :style="setRotation"></div>
+    <div class="hand minute" ref="minuteHand" :style="setRotation"></div>
+    <div class="hand second" ref="secondHand" :style="setRotation"></div>
     <div class="hour" v-for="(n, i) in 12" :key="i" :class='["num"+ i]'>{{n}}</div>
  </div>
 </template>
@@ -27,14 +53,17 @@
   background-color: white;
 }
 
-.point {
-  width: 20px;
-  height: 20px;
-  background-color: black;
-  border-radius: 50%;
+.clock-dial::after {
+  content: '';
   position: absolute;
+  background-color: black;
+  z-index: 11;
+  width: 15px;
+  height: 15px;
   top: 50%;
   left: 50%;
+  transform: translate(-50%, -50%);
+  border-radius: 50%;
 }
 
 .hour {
@@ -70,5 +99,37 @@
 .num9 {--rotation: 300deg}
 
 .num10 {--rotation: 330deg}
+
+.hand {
+  --rotation: 0;
+  position: absolute;
+  bottom: 50%;
+  left: 50%;
+  border: 1px solid white;
+  border-top-left-radius: 10px;
+  border-top-right-radius: 10px;
+  transform-origin: bottom;
+  z-index: 10;
+  transform: translateX(-50%) rotate(calc(var(--rotation) * 1deg));
+}
+
+.hand.second {
+  width: 5px;
+  height: 45%;
+  background-color: red;
+}
+
+.hand.minute {
+  width: 7px;
+  height: 40%;
+  background-color: black;
+}
+
+.hand.hour {
+  width: 10px;
+  height: 35%;
+  background-color: black;
+}
+
 
 </style>
